4a50b4fd07e85c19de4c79eac84a7c818a500e9e
vchimania <49272164+vchimania@users.noreply.github.com>
2021-02-16 15:07:27 +0530

    Add files via upload
    Initial Commit

---
+++ react-tmdb-app/client/src/components/Config.js
@@ -0,0 +1,14 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_KEY='cdb741ef4c87638e85e664d64a1bbc1a';
+export const API_URL= 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
+
+//Sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280'
+export const IMAGE_SIZE = 'w1280'
+
+// w92, w154, w185, w342, w500, w780, original
+ export const POSTER_SIZE = 'w500'
---
+++ react-tmdb-app/server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://vini_123:<7hCG8.c3DVGrqVd>@cluster0.nizos.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
